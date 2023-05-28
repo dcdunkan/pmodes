6af74f36692138590724362cf77744c6653a43c3
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2023-05-29 03:31:45 +0530

    fix: point types of vendored deps to vendored types

--- vendor/esm.sh/unicode-properties@1.4.1.js
+++ vendor/esm.sh/unicode-properties@1.4.1.js
@@ -1,3 +1,3 @@
-// @deno-types="https://esm.sh/v124/@types/unicode-properties@^1/index.d.ts"
+// @deno-types="./v124/@types/unicode-properties@1.3.0/index.d.ts"
 export * from "./unicode-properties@1.4.1.proxied.js";
 export { default } from "./unicode-properties@1.4.1.proxied.js";
