f88c0d7d2d74ace4391d4163a94d3f02a0c66dc3
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2023-06-14 03:52:11 +0530

    chore: fmt

--- user_id.ts
+++ user_id.ts
@@ -2,7 +2,7 @@ export class UserId {
   public static MAX_USER_ID = (1n << 40n) - 1n;
 
   constructor(public id = 0) {}
-  
+
   isValid() {
     return 0 < this.id && this.id <= UserId.MAX_USER_ID;
   }
